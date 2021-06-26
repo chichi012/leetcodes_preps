9c3e0ad014dacc21b5380fb307c9b12beb62bee1
Chidinma Afam <31688613+chichi012@users.noreply.github.com>
2021-06-26 20:27:17 +0100

    Time: 28 ms (98.18%), Space: 14.4 MB (29.17%) - LeetHub

---
+++ merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -0,0 +1,81 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        iter1 = l1
+        iter2 = l2
+        iter3 = sorted_l = ListNode()
+        
+        while iter1 is not None and iter2 is not None:
+            if iter1.val <= iter2.val:
+                iter3.next = iter1
+                iter1 = iter1.next
+                
+            elif iter1.val > iter2.val:
+                iter3.next = iter2
+                iter2 = iter2.next
+            iter3 = iter3.next
+                
+        if iter1 is not None:
+            iter3.next = iter1
+        elif iter2 is not None:
+            iter3.next = iter2
+            
+        return sorted_l.next
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         iter1 = l1
+#         iter2 = l2 
+#         l_list = ListNode()
+#         # l_list = iter_list = ListNode()
+#         iter_list = l_list
+#         while iter1 is not None and iter2 is not None:
+#             if iter1.val <= iter2.val:
+#                 iter_list.next = iter1
+#                 iter1 = iter1.next
+                
+#             else: #iter1.val > iter2.val:
+#                 iter_list.next = iter2
+#                 iter2 = iter2.next
+#             iter_list = iter_list.next
+                
+#         # return iter_list
+        
+#         if iter1 is not None:
+#             iter_list.next = iter1
+#         elif iter2 is not None:
+#             iter_list.next = iter2
+#         return l_list.next
+    
+                
+                
