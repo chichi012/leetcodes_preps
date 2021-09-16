2f0bd1f29ee8d4412e57e5c95c0f3ab13d4a2ac4
Chidinma Afamefuna <31688613+chichi012@users.noreply.github.com>
2021-09-16 20:14:40 +0100

    Time: 64 ms (9.15%), Space: 14.2 MB (85.33%) - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.py
+++ merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -7,52 +7,69 @@ class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         iter1 = l1
         iter2 = l2
-        iter3 = l_list = ListNode() 
+        iter3 = l3 = ListNode()
         
         while iter1 is not None and iter2 is not None:
             if iter1.val <= iter2.val:
                 iter3.next = iter1
-                iter1 = iter1.next         
-                
+                iter1 = iter1.next
             elif iter1.val > iter2.val:
-                iter3.next = iter2 
+                iter3.next = iter2
                 iter2 = iter2.next
             iter3 = iter3.next
-                
+            
         if iter1 is not None:
             iter3.next = iter1
         elif iter2 is not None:
             iter3.next = iter2
+        
+        return l3.next
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+#         iter1 = l1
+#         iter2 = l2
+#         iter3 = l_list = ListNode() 
+        
+#         while iter1 is not None and iter2 is not None:
+#             if iter1.val <= iter2.val:
+#                 iter3.next = iter1
+#                 iter1 = iter1.next         
+                
+#             elif iter1.val > iter2.val:
+#                 iter3.next = iter2 
+#                 iter2 = iter2.next
+#             iter3 = iter3.next
+                
+#         if iter1 is not None:
+#             iter3.next = iter1
+#         elif iter2 is not None:
+#             iter3.next = iter2
             
-        return l_list.next
+#         return l_list.next
             
         
         
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+      
         
 #         iter1 = l1
 #         iter2 = l2 
