1d0cdd14342f280d86de2da453816ac948c32b5c
Chidinma Afamefuna <31688613+chichi012@users.noreply.github.com>
2021-09-16 19:16:56 +0100

    Time: 72 ms (5.06%), Space: 20.4 MB (6.17%) - LeetHub

--- reverse-linked-list/reverse-linked-list.py
+++ reverse-linked-list/reverse-linked-list.py
@@ -6,35 +6,35 @@
 class Solution:
     
     def reverseList(self, head: ListNode) -> ListNode:
+     
+        # iter1 = head
+#         prev = None
+#         temp = None
         
-        iter1 = head
-        prev = None
-        temp = None
-        
-        while iter1 is not None:
-            temp =iter1.next
-            iter1.next = prev
-            prev = iter1
-            iter1 = temp
-        return prev
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         l_list = ListNode()
-#         iter_list = l_list
-#         iter1 = head
 #         while iter1 is not None:
-#             iter_list = iter1
-#             iter1 = iter1.next
-            
-#         return l_list
-       
+#             temp =iter1.next
+#             iter1.next = prev
+#             prev = iter1
+#             iter1 = temp
+#         return prev
+
+        return self.reverseListHelper(head)
+        
+        
+    def reverseListHelper(self,iter1,prev=None):
+        if not iter1:
+            return prev
+        temp = iter1.next
+        iter1.next = prev
+        prev = iter1
+        return self.reverseListHelper(temp,iter1)
+
+        
+        
+        
+        
+        
+        
+        
+
     
