e0fe6bc0daa7bf2d278ba5a95d5784b894154c07
Chidinma Afamefuna <31688613+chichi012@users.noreply.github.com>
2021-09-18 01:37:19 +0100

    Time: 1021 ms (20.79%), Space: 47.1 MB (50.74%) - LeetHub

--- palindrome-linked-list/palindrome-linked-list.py
+++ palindrome-linked-list/palindrome-linked-list.py
@@ -4,28 +4,79 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    def isPalindrome(self,head):
+        stack = []
+        iter1 = iter2 = length_iter = head
+        counter = 0
+        while length_iter is not None:
+            counter +=1
+            length_iter = length_iter.next
+        
+        if counter%2 ==0: #even length
+            step = 0
+            while step < counter//2:
+                stack.append(iter1.val)
+                iter1 = iter1.next
+                step+=1
+    
+            while iter1 is not None:
+                if iter1.val != stack.pop():
+                    return False
+                iter1 = iter1.next
+            return True         
+            
+        elif counter%2 !=0: #odd len:
+            step = 1
+            while step < counter//2 + 1:
+                stack.append(iter1.val)
+                iter1 = iter1.next
+                step+=1
+            iter1 = iter1.next
+        
+            while iter1 is not None:
+                if iter1.val != stack.pop():
+                    return False
+                iter1 = iter1.next
+            return True
+            
+        
+            
+            
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     """
     FIRST SOLUTION USING STACK TO REVERSE FIRST HALF AND COMPARE THE REMAINING OF THE LINKED LIST
     """
-    def isPalindrome(self,head):
-        fast = slow = head
-        stackpal = []
-        while fast.next!=None and fast.next.next !=None:
-            stackpal.append(slow.val)
-            slow = slow.next
-            fast = fast.next.next
+#     def isPalindrome(self,head):
+#         fast = slow = head
+#         stackpal = []
+#         while fast.next!=None and fast.next.next !=None:
+#             stackpal.append(slow.val)
+#             slow = slow.next
+#             fast = fast.next.next
         
-        if fast.next!=None:
-            stackpal.append(slow.val)
-        slow = slow.next
+#         if fast.next!=None:
+#             stackpal.append(slow.val)
+#         slow = slow.next
     
-        while slow is not None:
-            if slow.val ==stackpal.pop():
-                slow = slow.next
-            else:
-                return False
-        return True
+#         while slow is not None:
+#             if slow.val ==stackpal.pop():
+#                 slow = slow.next
+#             else:
+#                 return False
+#         return True
     
     
     
