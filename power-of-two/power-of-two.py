1d47506aec875bbd6940a939176c8550855ff28f
Chidinma Afamefuna <31688613+chichi012@users.noreply.github.com>
2021-09-19 01:38:08 +0100

    Time: 32 ms (71.67%), Space: 14.3 MB (38.60%) - LeetHub

--- power-of-two/power-of-two.py
+++ power-of-two/power-of-two.py
@@ -1,13 +1,27 @@
 import math
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
+        """SOLUTION WITH LOOPS"""
         if n <= 0:
             return False
-        x = math.log(n,2)
-        y = int(x)
-        if 2**y == n:
-            return True
-        else:
-            return False
+        while n%2 == 0:
+            n= n/2
+        return n==1
+        
+        
+        
+        
+        
+        
+        
+        """BRUTE FORCE WITHOUT LOOP / RECURSION"""
+        # if n <= 0:
+        #     return False
+        # x = math.log(n,2)
+        # y = int(x)
+        # if 2**y == n:
+        #     return True
+        # else:
+        #     return False
      
         
