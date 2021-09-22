523be9395288cbed06082c54f83324aa9d74519b
Chidinma Afamefuna <31688613+chichi012@users.noreply.github.com>
2021-09-22 10:21:15 +0100

    Time: 28 ms (86.63%), Space: 14.3 MB (49.94%) - LeetHub

--- powx-n/powx-n.py
+++ powx-n/powx-n.py
@@ -1,4 +1,20 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        elif n < 0:
+            n = -n 
+            x = 1/x
+            return pow(x,n)
+        else:
+            return pow((x*x),n/2) if n%2==0 else x*pow((x*x),n//2)
+
+        
+        
+        
+        
+        
+        
+        
         """BRUTE FORCE"""
-        return x**n
+        # return x**n
