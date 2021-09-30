da2359a28d729baf4d13af11b532dee4c3c37eef
Chidinma Afamefuna <31688613+chichi012@users.noreply.github.com>
2021-09-29 17:06:48 -0700

    Time: 352 ms (12.91%), Space: 22.4 MB (16.18%) - LeetHub

--- range-sum-of-bst/range-sum-of-bst.py
+++ range-sum-of-bst/range-sum-of-bst.py
@@ -6,25 +6,44 @@
 #         self.right = right
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        if root is None:
-            return
-    
-        given_range = [i for i in range(low,high+1)]
-        #using a BFS
-        queue = [root]
-        arr = []
         
+        if root is None:
+            return 
+        sums = 0
+        queue = [root]
         while len(queue) > 0:
             popped = queue.pop(0)
-            arr.append(popped.val)
+            if (popped.val >= low and popped.val <= high):
+                sums+= popped.val
             if popped.left:
                 queue.append(popped.left)
             if popped.right:
                 queue.append(popped.right)
-        sums = 0 
-        for i in arr:
-            if i in given_range:
-                sums+= i
-        return sums
+      
+
+        return sums  
+        
+        
+        
+#         if root is None:
+#             return
+    
+#         given_range = [i for i in range(low,high+1)]
+#         #using a BFS
+#         queue = [root]
+#         arr = []
+        
+#         while len(queue) > 0:
+#             popped = queue.pop(0)
+#             arr.append(popped.val)
+#             if popped.left:
+#                 queue.append(popped.left)
+#             if popped.right:
+#                 queue.append(popped.right)
+#         sums = 0 
+#         for i in arr:
+#             if i in given_range:
+#                 sums+= i
+#         return sums
       
         
