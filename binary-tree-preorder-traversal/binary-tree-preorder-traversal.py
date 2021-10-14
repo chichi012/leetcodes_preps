76722a72066c56275d1f80ff57a90ac6f7fdde58
Chidinma Afamefuna <31688613+chichi012@users.noreply.github.com>
2021-10-14 13:29:01 -0700

    Time: 41 ms (26.48%), Space: 14.1 MB (91.35%) - LeetHub

--- binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
+++ binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
@@ -6,14 +6,52 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        arr = []
-        self.preorderTraversalHelper(root,arr)
-        return arr
-    def preorderTraversalHelper(self,node,arr):
-        if node is None:
-            return
-        arr.append(node.val)
-        if node.left:
-            self.preorderTraversalHelper(node.left,arr)
-        if node.right:
-            self.preorderTraversalHelper(node.right,arr)
+        """one line solution"""
+        return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right) if root else []
+                
+#         """ITERATIVE SOLUTION 2"""
+#         stack = [root]
+#         res = []
+
+#         while len(stack) > 0: 
+#             curr = stack.pop()
+#             if curr is not None:
+#                 res.append(curr.val)
+#                 stack.append(curr.right)
+#                 stack.append(curr.left)
+#         return res
+            
+        
+
+        
+        """ITERATIVE SOLUTION 1"""
+#         stack = []
+#         res = []
+#         curr = root
+#         while curr is not None or len(stack) > 0:   
+#             while curr is not None:
+#                 res.append(curr.val)
+#                 stack.append(curr)
+#                 curr = curr.left
+            
+#             curr = stack.pop()
+#             curr = curr.right
+            
+#         return res
+            
+        
+
+        
+        """RECURSIVE SOLUTION"""
+        
+    #     arr = []
+    #     self.preorderTraversalHelper(root,arr)
+    #     return arr
+    # def preorderTraversalHelper(self,node,arr):
+    #     if node is None:
+    #         return
+    #     arr.append(node.val)
+    #     if node.left:
+    #         self.preorderTraversalHelper(node.left,arr)
+    #     if node.right:
+    #         self.preorderTraversalHelper(node.right,arr)
