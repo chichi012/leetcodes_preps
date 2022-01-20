9ac57f0a1853627e5ac9a4110bda69ec7074c8c1
Chidinma Afamefuna <31688613+chichi012@users.noreply.github.com>
2022-01-20 01:25:31 +0000

    Time: 55 ms (27.36%), Space: 14.3 MB (31.45%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -5,51 +5,53 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        iter1 = list1
-        iter2 = list2
-        iter3 = l_list = ListNode()
         
-        self.mergeTwoListsHelper(iter1,iter2,iter3)
-        
-        return l_list.next;
-    
-    def mergeTwoListsHelper(self,iter1,iter2,iter3):
-        if iter1 is None and iter2 is None:
-            return 
-        elif iter1 is not None and iter2 is None:
-            iter3.next = iter1
-            return
-        elif iter2 is not None and iter1 is None:
-            iter3.next = iter2 
-            return
-        if iter1.val <= iter2.val:
-            iter3.next = iter1
-            self.mergeTwoListsHelper(iter1.next, iter2, iter3.next)
-        elif iter1.val > iter2.val:
-            iter3.next = iter2 
-            self.mergeTwoListsHelper(iter1, iter2.next, iter3.next)
-            
+        """this worked"""
 #         iter1 = list1
 #         iter2 = list2
-#         node = dummy_node = ListNode()
+#         iter3 = l_list = ListNode()
+        
+#         self.mergeTwoListsHelper(iter1,iter2,iter3)
+        
+#         return l_list.next;
+    
+#     def mergeTwoListsHelper(self,iter1,iter2,iter3):
 #         if iter1 is None and iter2 is None:
+#             return 
+#         elif iter1 is not None and iter2 is None:
+#             iter3.next = iter1
 #             return
-#         while iter1 is not None and iter2 is not None:
-#             if iter1.val <= iter2.val:
-#                 node.next = iter1
-#                 iter1 = iter1.next
-#             elif iter1.val > iter2.val:
-#                 node.next = iter2
-#                 iter2 = iter2.next
-#             node = node.next
+#         elif iter2 is not None and iter1 is None:
+#             iter3.next = iter2 
+#             return
+#         if iter1.val <= iter2.val:
+#             iter3.next = iter1
+#             self.mergeTwoListsHelper(iter1.next, iter2, iter3.next)
+#         elif iter1.val > iter2.val:
+#             iter3.next = iter2 
+#             self.mergeTwoListsHelper(iter1, iter2.next, iter3.next)
             
-#         while iter1 is not None and iter2 is None:
-#             node.next = iter1
+        iter1 = list1
+        iter2 = list2
+        node = dummy_node = ListNode()
+        if iter1 is None and iter2 is None:
+            return
+        while iter1 is not None and iter2 is not None:
+            if iter1.val <= iter2.val:
+                node.next = iter1
+                iter1 = iter1.next
+            elif iter1.val > iter2.val:
+                node.next = iter2
+                iter2 = iter2.next
+            node = node.next
+            
+        if iter1 is not None and iter2 is None:
+            node.next = iter1
             
                 
-#         while iter2 is not None and iter1 is None:
-#             node.next = iter2
+        if iter2 is not None and iter1 is None:
+            node.next = iter2
             
-#         return dummy_node.next
+        return dummy_node.next
             
         
