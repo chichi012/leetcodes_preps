a47975e72ec20813a94c978cdf7c2b0d0093df38
Chidinma Afamefuna <31688613+chichi012@users.noreply.github.com>
2022-01-22 17:32:07 +0000

    Time: 82 ms (5.16%), Space: 14.3 MB (16.26%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
@@ -5,26 +5,55 @@
 #         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+              
         if not head:
             return
         if not head.next:
             return head
-        iter1 = head
-        dummy = prev = ListNode(0,head)
-        while iter1 and iter1.next:
-            #get pointers for next pair to swap
-            nextPair= iter1.next.next
-            iter2 = iter1.next
+        
+        dummy = ListNode(0,head)
+        self.swapPairsHelper(dummy)
+        return dummy.next
+        
+    def swapPairsHelper(self,node):
+        if not node.next:
+            return
+        if not node.next.next:
+            return
+        else:
+            iter1 = node.next
+            iter2 = node.next.next
+            temp = iter1
             
             #swapping
-            iter2.next = iter1
-            iter1.next = nextPair
-            prev.next = iter2
+            node.next ,iter2.next, iter1.next = iter2,iter1,iter2.next
+            self.swapPairsHelper(temp)
             
-            #update pointers
-            prev = iter1
-            iter1 = iter1.next
+        
+        
+        
+        
+
+#         if not head:
+#             return
+#         if not head.next:
+#             return head
+#         iter1 = head
+#         dummy = prev = ListNode(0,head)
+#         while iter1 and iter1.next:
+#             #get pointers for next pair to swap
+#             nextPair= iter1.next.next
+#             iter2 = iter1.next
+            
+#             #swapping
+#             iter2.next = iter1
+#             iter1.next = nextPair
+#             prev.next = iter2
+            
+#             #update pointers
+#             prev = iter1
+#             iter1 = iter1.next
             
             
-        return dummy.next
+#         return dummy.next
         
