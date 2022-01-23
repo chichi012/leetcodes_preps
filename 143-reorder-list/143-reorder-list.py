dc31b8c390a921b180b77e6f1263a63d5f66f781
Chidinma Afamefuna <31688613+chichi012@users.noreply.github.com>
2022-01-23 23:06:52 +0000

    Time: 88 ms (88.24%), Space: 23.2 MB (78.93%) - LeetHub

---
+++ 143-reorder-list/143-reorder-list.py
@@ -0,0 +1,119 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+import collections
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        
+        
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        """STRATEGY
+        1. get to middle of linkedlist and separate them
+        2. reverse second half
+        3. re-arrange pointers
+        """ 
+        
+#         step 1
+        
+        fast = slow = head
+        while fast.next is not None  and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        new_l = slow.next
+        slow.next = None
+        
+        
+#      step 2
+        iter1 = new_l
+        prev = None
+        temp = None
+        while iter1 is not None:
+            temp = iter1.next
+            iter1.next = prev
+            prev = iter1
+            iter1 = temp
+        # print("head left =====",head)
+        # print("reversed =====",prev)
+        #note that reversed list is prev
+        
+         
+#             step 3
+
+      
+        first_half = head
+        second_half = prev
+        temp = None
+        
+        while second_half is not None:
+            temp = first_half.next
+            first_half.next = second_half
+            first_half = temp
+            
+            temp  = second_half.next
+            second_half.next = first_half
+            second_half = temp
+            
+            
+        # print("final ans =======", head)
+
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         sett = set()
+#         iter1 = head
+#         tail = None
+#         while iter1:
+#             sett.add(iter1)
+#             iter1 = iter1.next
+        
+#         dummy = ListNode()
+#         dummy.next = head
+#         iter2 = dummy
+        
+#         while iter2.next in sett:
+#             iter2.next = 
+            
+#     def tail(self,val):
+#         new_node = ListNode(val)
+#         if self.head is None:
+#             self.head = self.tail = new_node
+#             return
+#         self.tail.next = new_node
+#         self.tail = new_node
+
+
+
+
+
+
+
+
+
+#         stack = collections.deque([])
+#         iter1 = head
+#         while iter1:
+#             stack.append(iter1)
+#             iter1 = iter1.next
+#         iter2 = head
+#         while stack:
+#             temp = iter2.next
+#             iter2.next = stack.popleft()
+#             iter2 = iter2.next
+#             iter2.next = temp
+            
+            
+        
