c234376c2da1e1f340df6d31a0129ed2f6c95f92
Chidinma Afamefuna <31688613+chichi012@users.noreply.github.com>
2022-01-23 01:03:27 +0000

    Time: 125 ms (7.60%), Space: 17.2 MB (69.33%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.py
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.py
@@ -5,10 +5,13 @@
 #         self.next = next
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        # this is O(N) time and O(1) space compexity
+        
+        
         prev = dummy = ListNode(0,head)
         iter1 = head
         while iter1:
-            # prev.next = iter1
+            # prev.next = iter1 can be commented out or not
             if iter1.val == val:
                 prev.next = iter1.next
                 iter1 = iter1.next
