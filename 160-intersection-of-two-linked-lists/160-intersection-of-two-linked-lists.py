8895decb6615c6f76ef0d70752e900915cd5cdcc
Chidinma Afamefuna <31688613+chichi012@users.noreply.github.com>
2022-01-23 01:02:04 +0000

    Time: 238 ms (24.72%), Space: 29.4 MB (54.94%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
@@ -6,20 +6,66 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        s = set()
+        #this is O(N+M) time and O(1) space
+        #Loop through each Llist, get their lengths. move longer llist by their difference
+        #then return node where they meet 
+        
         iter1 = headA
         iter2 = headB
+        a,b = 0,0
         
-        while iter1 is not None:
-            s.add(iter1)
+        while iter1:
+            a+=1
             iter1 = iter1.next
             
-        while iter2 is not None:
-            if iter2 in s:
-                return iter2
-            s.add(iter2)
+        while iter2:
+            b+=1
             iter2 = iter2.next
             
-        return
+        if a>b:
+            diff = a-b
+            #start at beginning again
+            long = headA
+            short = headB
+        else:
+            diff = b-a
+            long = headB
+            short = headA
+            
+        for i in range(diff): #move longer llist by difference
+            long = long.next
+        
+        while long != short:
+            long = long.next
+            short = short.next
+        
+        return short
+            
+    
+            
+        
+        
+        
+        
+        
+        
+#         Solution 2
+        
+        #this is O(N+M) time and space
+#         s = set()
+#         iter1 = headA
+#         iter2 = headB
+        
+#         while iter1 is not None:
+#             s.add(iter1)
+#             iter1 = iter1.next
+            
+#         while iter2 is not None:
+#             if iter2 in s:
+#                 return iter2
+#             s.add(iter2)
+#             iter2 = iter2.next
+            
+#         return
         
         
