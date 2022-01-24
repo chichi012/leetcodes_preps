5756b10c4bff891117978afa104c4410a4ec71c6
Chidinma Afamefuna <31688613+chichi012@users.noreply.github.com>
2022-01-24 00:02:32 +0000

    Time: 1363 ms (9.69%), Space: 39.3 MB (86.56%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -0,0 +1,49 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+#         get to half
+#         reverse last half
+#         iterate from beginning and last half if both are equal else not a palindrome
+        iter1 = head
+        iter2 = head
+        while iter2.next and iter2.next.next:
+            iter1 = iter1.next
+            iter2 = iter2.next.next
+            
+        new_list = iter1.next #starts second list
+        iter1.next = None #ends list of head at firsthalf
+        
+        
+        #reverse last half
+        iter1 = new_list
+        temp = None
+        prev = None
+        
+        while iter1:
+            temp = iter1.next 
+            iter1.next = prev
+            prev = iter1
+            iter1 = temp
+        
+        
+        # iterate from beginning and last half if both are equal else not a palindrome
+        a = head
+        b = prev
+        while b:
+            if b.val != a.val:
+                return False
+            a = a.next
+            b = b.next
+        return True
+            
+            
+            
+            
+            
+#         solution 2
+#         use stack
+#         pop from stack while iterating llist if same else not a palindrome
