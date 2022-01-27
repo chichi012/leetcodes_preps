d9d9036ec0c3ba8b9ccac7ebf1edc5825d7f9c22
Chidinma Afamefuna <31688613+chichi012@users.noreply.github.com>
2022-01-27 23:32:49 +0000

    Time: 1093 ms (29.83%), Space: 132.3 MB (5.15%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -4,42 +4,71 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+    # def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        
+        #O(N) time and O(1) space. Takes longer time to run
+        
 #         get to half
 #         reverse last half
 #         iterate from beginning and last half if both are equal else not a palindrome
+#         iter1 = head
+#         iter2 = head
+#         while iter2.next and iter2.next.next:
+#             iter1 = iter1.next
+#             iter2 = iter2.next.next
+            
+#         new_list = iter1.next #starts second list
+#         iter1.next = None #ends list of head at firsthalf
+        
+        
+#         #reverse last half
+#         iter1 = new_list
+#         temp = None
+#         prev = None
+        
+#         while iter1:
+#             temp = iter1.next 
+#             iter1.next = prev
+#             prev = iter1
+#             iter1 = temp
+        
+        
+#         # iterate from beginning and last half if both are equal else not a palindrome
+#         a = head
+#         b = prev
+#         while b:
+#             if b.val != a.val:
+#                 return False
+#             a = a.next
+#             b = b.next
+#         return True
+            
+        
+        # #O(N) time and O(N) space. Takes Shorter time to run but more space
+    # """SOLUTION USING RECURSION 
+    # keep a pointer at fake node with its .next = head, recurse through linkedlist to end....on returns, compare returned value with moving pointer at head..return true if same"""
+    
+    
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        start = ListNode()
+        start.next = head;
         iter1 = head
-        iter2 = head
-        while iter2.next and iter2.next.next:
-            iter1 = iter1.next
-            iter2 = iter2.next.next
-            
-        new_list = iter1.next #starts second list
-        iter1.next = None #ends list of head at firsthalf
+        result = self.isPalindromeHelper(start,iter1)
+        return result
         
-        
-        #reverse last half
-        iter1 = new_list
-        temp = None
-        prev = None
-        
-        while iter1:
-            temp = iter1.next 
-            iter1.next = prev
-            prev = iter1
-            iter1 = temp
-        
-        
-        # iterate from beginning and last half if both are equal else not a palindrome
-        a = head
-        b = prev
-        while b:
-            if b.val != a.val:
+    
+    def isPalindromeHelper(self,start,iter1):
+        if iter1 is None:
+            return True
+        else:
+            result = self.isPalindromeHelper(start,iter1.next)
+            if result == False or iter1.val != start.next.val:
                 return False
-            a = a.next
-            b = b.next
-        return True
-            
-            
+            else:
+                start.next = start.next.next
+                return True
+    
+    
+                
     
 
