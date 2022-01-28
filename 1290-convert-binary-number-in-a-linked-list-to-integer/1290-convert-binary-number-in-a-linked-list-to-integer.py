5885fc4b3fb84a9f82638b710c8a52ed7c591783
Chidinma Afamefuna <31688613+chichi012@users.noreply.github.com>
2022-01-28 01:58:15 +0000

    Time: 32 ms (75.02%), Space: 14 MB (88.68%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.py
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.py
@@ -5,23 +5,34 @@
 #         self.next = next
 class Solution:
     def getDecimalValue(self, head: ListNode) -> int:
+        #Approach 2: using bit manipulation
+        #Time complexity:O(N) , Space: O(1)
+        num = head.val
+        while head.next:
+            num = (num << 1) | head.next.val #10 + 0 = 10 converted to 2
+            head = head.next
+        return num
+        
+        
+#         O(N) time and O(1) space
+
         #approach 1: get length of binary number, and convert the usual way
-        length = 0
-        iter1 = head
-        while iter1:
-            length +=1
-            iter1 = iter1.next
+#         length = 0
+#         iter1 = head
+#         while iter1:
+#             length +=1
+#             iter1 = iter1.next
         
-        #since it starts at 0 index when expanding, subtract 1
-        indices = length - 1 
+#         #since it starts at 0 index when expanding, subtract 1
+#         indices = length - 1 
         
-        iter1 = head
-        summ = 0
-        while iter1:
-            summ += iter1.val * (2**(indices))
-            iter1 = iter1.next
-            indices -=1
+#         iter1 = head
+#         summ = 0
+#         while iter1:
+#             summ += iter1.val * (2**(indices))
+#             iter1 = iter1.next
+#             indices -=1
             
-        return summ
+#         return summ
         
         
