2a27580c9fbc644fbe7fc93f9505c81f3fd545de
Chidinma Afamefuna <31688613+chichi012@users.noreply.github.com>
2022-01-28 02:16:06 +0000

    Time: 162 ms (18.38%), Space: 17.3 MB (58.40%) - LeetHub

--- 382-linked-list-random-node/382-linked-list-random-node.py
+++ 382-linked-list-random-node/382-linked-list-random-node.py
@@ -5,18 +5,42 @@
 #         self.next = next
 import random
 class Solution:
-
+    #Solution 2: RESERVOIR SAMPLING
+    #Space complexity is O(1)
+    #Time complexity is O(1)
     def __init__(self, head: Optional[ListNode]):
         self.head = head
         
+    #Time complexity is O(N) where NN is the number of elements in the input list.
     def getRandom(self) -> int:
-        l =[]
-        iter1 = self.head
-        while iter1:
-            l.append(iter1.val)
-            iter1 = iter1.next
+        scope = 1 
+        chosen_value = 0
+        curr = self.head
 
-        return random.choice(l)
+        while curr:
+            # decide whether to include the element in reservoir
+            if random.random() < 1 / scope:
+                chosen_value = curr.val
+            # move on to the next node
+            curr = curr.next
+            scope += 1
+        return chosen_value
+
+
+#  Solution 1: convert the linked list into an array. Uses extra space and cant cater to large list
+#O(N) space and time complexity
+
+#     def __init__(self, head: Optional[ListNode]):
+#         self.head = head
+        
+#     def getRandom(self) -> int:
+#         l =[]
+#         iter1 = self.head
+#         while iter1:
+#             l.append(iter1.val)
+#             iter1 = iter1.next
+
+#         return random.choice(l)
 
 
 
