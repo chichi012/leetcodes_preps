975feb2802f00ebb40e1b7dc68a73ac18c99cbdd
Chidinma Afamefuna <31688613+chichi012@users.noreply.github.com>
2022-02-02 00:22:01 +0000

    Time: 468 ms (57.65%), Space: 25.9 MB (45.51%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,5 +1,7 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        #O(N) space and time complexity.
         dicct  = dict()
         for item in nums:
             if item not in dicct:
@@ -10,3 +12,12 @@ class Solution:
             if value >=2:
                 return True
                 
+
+        
+#         Solution 2
+#         O(N logN) Time complexity and O(1) space
+        # num = sorted(nums)
+        # for i in range(len(num)-1):
+        #     if num[i] == num[i+1]:
+        #         return True
+        # return False
