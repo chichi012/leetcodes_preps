299e65990c7457a490099c83f93a6e62bb72b66d
Chidinma Afamefuna <31688613+chichi012@users.noreply.github.com>
2022-02-06 23:23:41 +0000

    Time: 398 ms (52.87%), Space: 16.6 MB (84.23%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,22 +1,31 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # https://www.youtube.com/watch?v=pV2kpPD66nE solution 2
-        # solution 1
-        if not grid:
-            return 0
-        num_islands = 0
-        for r in range(len(grid)):
-            for c in range(len(grid[r])):
-                if grid[r][c] =='1':
-                    self.dfs(grid,r,c)
-                    num_islands+=1
+#      Approach
+#      1. Loop through grid[row][col] for "1"s. Call a depth first search on it 
+#      2. Mark grid[row][col] as seen then check its neighbours, Call a depth first search on "1"s encountered
+#      3. After dfs call, increment num_islands count and return 
+        """
+      ["1","1","1","1","0"],
+      ["1","1","0","1","0"],
+      ["1","1","0","0","0"],
+      ["0","0","1","1","0"]
+        """
+        num_islands  = 0
+        for row_index in range(len(grid)):
+            for col_index in range(len(grid[row_index])):
+                if grid[row_index][col_index] == "1":
+                    print(row_index)
+                    self.numIslandsHelper(grid,row_index,col_index)
+                    num_islands +=1
         return num_islands
-        
-        
-    def dfs(self,grid,r,c):
-        grid[r][c] = '0'  #marking it as seen so it doesnt get counted again
+
+    def numIslandsHelper(self,grid,r,c):
+        #mark as seen
+        grid[r][c] = "0"
+        #next check neighbours [up,down,left,right]
         neighbours = [(r-1,c),(r+1,c),(r,c-1),(r,c+1)]
         for row,col in neighbours:
-            if row >=0 and col>=0 and row<len(grid) and col <len(grid[row]) and   grid[row][col] == '1':
-                self.dfs(grid,row,col)
+            if row >=0 and col >=0 and row < len(grid) and col<len(grid[row]) and grid[row][col] == "1":
+                self.numIslandsHelper(grid,row,col)
+                
         
