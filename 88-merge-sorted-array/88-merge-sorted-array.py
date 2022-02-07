9b7ed8d429389b0ab351f42d6a164b3e92aa1597
Chidinma Afamefuna <31688613+chichi012@users.noreply.github.com>
2022-02-07 17:00:19 +0000

    Time: 36 ms (85.28%), Space: 13.9 MB (98.23%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -3,6 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.      
         """
+#         O(N) time and O(1) Space
         pointer = m+n-1
         arr = [None]*len(nums1)
         
@@ -32,25 +33,6 @@ class Solution:
 #                 n -= 1
 #         nums1[:n] = nums2[:n]
 
-
-        
-#         p1 = p2 = 0
-#         if m == 0 and n!=0:
-#             nums1[p1] = nums2[p2] 
-        
-#         if n and m:
-#             for _ in range(m):
-#                 if nums1[p1] <= nums2[p2]:
-#                 # and nums1[p1] !=0: 
-#                     p1+=1
-#                 else:
-#                     nums1[p1],nums2[p2] = nums2[p2],nums1[p1]
-#                     p2+=1
-
-#             for _ in range(p1,len(nums1)) :
-#                 nums1[p1] = nums2[p2]
-#                 p1+=1
-#                 p2+=1
 
 
 
