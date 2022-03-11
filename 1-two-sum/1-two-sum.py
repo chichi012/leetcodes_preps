e9e69bce3123d1fcdc49e89b6b9ca83ecb687124
Chidinma Afamefuna <31688613+chichi012@users.noreply.github.com>
2022-03-11 10:46:30 +0000

    Time: 79 ms (74.49%), Space: 15.2 MB (55.40%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -3,7 +3,7 @@ class Solution:
         
         #O(N) time and space cmplexity
         d = dict()
-        i = 0
+        i = 0 #store their index
         for item in nums:
             d[item] = i
             i+=1
@@ -12,7 +12,7 @@ class Solution:
         
         for i in range(len(nums)):
             complement = target - nums[i]
-            if complement in d and d[complement] != i:
+            if complement in d and d[complement] != i: #ensuring its not duplicated numbers
                 return [i,d[complement]]
             
    
