dba1128f18e0b8436057452f1bf51d014416da52
Chidinma Afamefuna <31688613+chichi012@users.noreply.github.com>
2022-03-16 17:45:31 +0000

    Time: 132 ms (65.09%), Space: 15 MB (30.13%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,15 +1,19 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        #brute force
+        # Brute force - O(n) time, O(n) space
 #         for i in range(len(numbers)):
 #             for j in range(i+1,len(numbers)):
 #                 if numbers[i] + numbers[j] == target:
 #                     return [i+1,j+1]
 #                 elif numbers[i] + numbers[j] > target:
 #                     continue
+
+# or using the dictionary mehtod is brute force as well since its O(n) time, O(n) space
+# https://leetcode.com/problems/two-sum/
+
                     
     
-        #TWO POINTER METHOD
+        #TWO POINTER METHOD  O(n) time, O(1) space
         left = 0
         right =  len(numbers)-1
         
@@ -20,7 +24,7 @@ class Solution:
             if sums >target:
                 right -=1
             elif sums == target:
-                return [left+1,right+1]
+                return [left+1,right+1] #since array is 1-indexed and ours is 0-indexed
             
         return []
         
