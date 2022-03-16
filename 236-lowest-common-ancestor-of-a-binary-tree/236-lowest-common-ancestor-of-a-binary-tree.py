14d62051b486f8bf67970e5c8cd5863ddbf4a171
Chidinma Afamefuna <31688613+chichi012@users.noreply.github.com>
2022-03-16 19:12:04 +0000

    Time: 115 ms (37.04%), Space: 26.4 MB (35.57%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -9,7 +9,7 @@ class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
         """
-        Using Depth First Search Algorithm
+        Using Depth First Search Algorithm, Once we reach the desired nodes p and q, we can backtrack and find the lowest common ancestor.
         
         Complexity Analysis
 
@@ -18,16 +18,21 @@ Time Complexity: O(N), where N is the number of nodes in the binary tree. In the
 Space Complexity: O(N). This is because the maximum amount of space utilized by the recursion stack would be N since the height of a skewed binary tree could be N.
         """
         self.result = None
+
     
         def lowestCommonAncestorHelper(node):
             if not node:
-                return False
+                return 0
 
             left = lowestCommonAncestorHelper(node.left)
 
             right = lowestCommonAncestorHelper(node.right)
     
-            mid = node == p or node == q
+            if node == p or node == q:
+                mid = 1 
+            else:
+                mid = 0
+            
             
             # If any two of the three flags left, right or mid become True.
             if mid + left + right >= 2:
