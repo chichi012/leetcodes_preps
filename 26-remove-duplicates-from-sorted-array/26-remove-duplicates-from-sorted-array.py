b8c6edcbfe9f944a734ea98f08fc479eb9e77537
Chidinma Afamefuna <31688613+chichi012@users.noreply.github.com>
2022-03-17 02:55:06 +0000

    Time: 123 ms (59.11%), Space: 15.5 MB (70.90%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,54 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        """
+        USING TWO POINTERS
+        Strategy: [0,0,1,2,2,3,3,4,5,6,6]
+        
+        - first element will always come first so start two pointers at second element index arr[1]
+        - left, right = 1, 1 
+        
+         [0,0,1,2,2,3,3,4,5,6,6]
+            r
+            l
+           
+        - left will point to the position that needs to be filled based on right's comparison with its previous neighbor
+        
+        [0,0,1,2,2,3,3,4,5,6,6] : arr[r] != arr[r-1] (meaning its a new number) so we fill in arr[l] = arr[r], then increament both pointers
+           r -
+           l
+        
+        [0,1,1,2,2,3,3,4,5,6,6] arr[r] != arr[r-1] so we fill in arr[l] = arr[r], then increament both pointers
+             l
+             r
+            
+        [0,1,2,2,2,3,3,4,5,6,6] since arr[r] == arr[r-1] so we keep moving forward with r and dont update left
+               l
+               r r
+               
+        at the end, return the left pointer(exactly its number) since that wil give the "k" distinct elements
+        
+        """
+        left = 1
+        
+        for right in range(1,len(nums)):
+            #enter condition only if tree, else increment right pointer
+            if nums[right] != nums[right-1]:
+                nums[left] = nums[right]
+                left +=1
+            #the for loop takes care of the increment of right pointer
+                
+        return left
+                
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
